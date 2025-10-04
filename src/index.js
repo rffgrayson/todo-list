0974f12fc47d222f29969f0b3515b93cae2b7ad3
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-10-04 16:33:55 +0800

    create class for folder
    test specifically with console log status

--- src/index.js
+++ src/index.js
@@ -17,3 +17,30 @@ const todo1 = new Todo("Study", "Finish Odin Project assignment", "2025-10-10");
 console.log(todo1);
 todo1.changeStatus();
 console.log(todo1);
+console.log("todo1.status:", todo1.status);
+
+
+class Folder {
+    constructor(category) {
+        this.category = category;
+        this.list = [];
+    }
+
+    addTodo (todo) {
+        this.list.push(todo);
+    }
+}
+
+
+const file1 = new Folder("test");
+console.log(file1);
+file1.addTodo(todo1);
+console.log(file1);
+console.log("todo1.status:", todo1.status);
+console.log("folder todo status:", file1.list[0].status);
+
+todo1.changeStatus();
+console.log(todo1);
+console.log(file1);
+console.log("todo1.status:", todo1.status);
+console.log("folder todo status:", file1.list[0].status);
