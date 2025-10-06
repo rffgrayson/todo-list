1dd617fa88cf8d36668c126badb28a283eb44fea
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-10-06 19:13:03 +0800

    add function to add the todo inside the dashboard

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ class Todo {
         this.description = description;
         this.due = due;
         this.status = false;
+        this.id = "todo" + crypto.randomUUID();
     }
 
     changeStatus () {
@@ -32,8 +33,11 @@ class Folder {
     addTodo (todo) {
         this.list.push(todo);
     }
-}
 
+    removeTodo (todo) {
+        this.list.remove(todo);
+    }
+}
 
 const file1 = new Folder("test");
 console.log(file1);
@@ -48,4 +52,32 @@ console.log(file1);
 console.log("todo1.status:", todo1.status);
 console.log("folder todo status:", file1.list[0].status);
 
+const fileBtn = document.querySelector("#file-btn");
+
+fileBtn.addEventListener("click", ()=> {
+
+});
     
+function addTodoLibrary (todo) {
+    console.log(todo);
+    const newDiv = document.createElement("div");
+    newDiv.classList.toggle(`todo`);
+    newDiv.id = `${todo.id}`;
+    newDiv.innerHTML += `
+    <div class="todo-item" data-priority="high">
+      <div class="item-section">
+        <span class="todo-title">${todo.title}</span>
+        <span class="todo-description">${todo.description}</span>
+        <span class="todo-date">Due:${todo.due}</span>
+      </div>
+      <button class="delete-btn">x
+        </button>  
+    </div>
+    `
+
+    const todoSection = document.querySelector(".todo-list");
+    todoSection.appendChild(newDiv);
+}
+
+addTodoLibrary(todo1);
+
