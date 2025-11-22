dc5ce5e1c4d4e559f2786ada435da511151c1ad0
Igloopotato <141108687+Igloopotato@users.noreply.github.com>
2025-11-22 11:23:57 +0800

    change the date to relative date using package date-fns

--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,4 @@
-
+import { parseISO, formatDistanceToNow } from "date-fns";
 class Form {
     constructor(formId) {
         this.form = document.querySelector(`#${formId}`);
@@ -104,6 +104,9 @@ class UI {
         div.id = todo.id;
         div.dataset.folderId = todo.folderId;
         div.dataset.priority = todo.priority;
+
+        const dueDate = parseISO(todo.due);
+        const relativeDate = formatDistanceToNow(dueDate);
         
         div.innerHTML += `
           <div class="item-section">
@@ -118,7 +121,7 @@ class UI {
                 </div>
                 <div class="todo-footer-due">
                     <svg fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M12,14a1,1,0,1,0-1-1A1,1,0,0,0,12,14Zm5,0a1,1,0,1,0-1-1A1,1,0,0,0,17,14Zm-5,4a1,1,0,1,0-1-1A1,1,0,0,0,12,18Zm5,0a1,1,0,1,0-1-1A1,1,0,0,0,17,18ZM7,14a1,1,0,1,0-1-1A1,1,0,0,0,7,14ZM19,4H18V3a1,1,0,0,0-2,0V4H8V3A1,1,0,0,0,6,3V4H5A3,3,0,0,0,2,7V19a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V7A3,3,0,0,0,19,4Zm1,15a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V10H20ZM20,8H4V7A1,1,0,0,1,5,6H19a1,1,0,0,1,1,1ZM7,18a1,1,0,1,0-1-1A1,1,0,0,0,7,18Z"></path></g></svg>
-                    <span>${todo.due}</span>
+                    <span>${relativeDate}</span>
                 </div>
             </div>
           </div>
